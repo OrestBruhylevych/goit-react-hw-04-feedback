001c12026a8b40f8604ed448a5b40ed532c9f8f9
Bryhilevych Orest <91126054+OrestBruhylevych@users.noreply.github.com>
2022-07-13 19:17:15 +0300

    add hook

--- src/components/FeedbackApp/FeedbackApp.jsx
+++ src/components/FeedbackApp/FeedbackApp.jsx
@@ -1,63 +1,61 @@
 import { Box } from 'components/Box';
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 
-export default class FeedbackApp extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function FeedbackApp() {
+  const options = ['good', 'neutral', 'bad'];
 
-  onLeaveFeedback = e => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = e => {
     const buttonName = e.target.name;
 
-    this.setState(prevState => {
-      return { [buttonName]: (prevState[buttonName] += 1) };
-    });
+    switch (buttonName) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     const total = good + neutral + bad;
-
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     const procentPositiveFeedback = (good * 100) / total;
 
     return procentPositiveFeedback;
   };
 
-  options = () => {
-    const options = this.state;
-    const optionsArray = Object.keys(options);
-    return optionsArray;
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <Box p={4} background="white" width="400px" ml="auto" mr="auto">
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.options()}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        </Section>
-      </Box>
-    );
-  }
+  return (
+    <Box p={4} background="white" width="400px" ml="auto" mr="auto">
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      </Section>
+    </Box>
+  );
 }
